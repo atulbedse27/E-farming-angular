e3296a08eba9784cd7ef5b37c1025c0079fd252a
atulbedse27 <atulbedse27@gmail.com>
2019-11-27 20:02:48 +0530

    Folder Structure For Angular App Is Created

---
+++ EfarmingAngular/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { MaterialModule } from './material/material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ EfarmingAngular/src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+
+const materialModule = []
+
+@NgModule({
+  
+  imports: [materialModule],
+  exports: [materialModule]
+})
+export class MaterialModule { }
